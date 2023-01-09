705743506a50da8a6cd77bd261c9c56c52a0b829
ymc182 <ymc182@gmail.com>
2023-01-10 02:01:18 +1100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "@near-wallet-selector/modal-ui/styles.css";
+
+import "react-toastify/dist/ReactToastify.css";
 import { Buffer } from "buffer";
+import { NearProvider } from "./context/NearProvider";
+import { ToastContainer } from "react-toastify";
 
 // @ts-ignore
 globalThis.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<NearProvider>
+			<App />
+			<ToastContainer />
+		</NearProvider>
 	</React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
+import nodePolyfills from "vite-plugin-node-stdlib-browser";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	// ...other config settings
+	plugins: [react(), nodePolyfills()],
 });
